0dddfc1cec42acd489064923627350c51786056a
SK <sarthakkarandikar03@gmail.com>
2023-01-03 00:31:28 +0000

    Jan 3, 2023, 6:01 AM

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,12 @@
 import classes from './User.module.css';
 
 class User extends Component{
-  render()
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>
+  }
 }
 const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
+  ;
 };
 
 export default User;
