50e3747ceb64883ea1ed77f339618a21cebca497
SK <sarthakkarandikar03@gmail.com>
2023-01-03 01:17:44 +0000

    Jan 3, 2023, 6:47 AM

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -23,7 +23,7 @@ class UserFinder extends Component{
     }
 
     componentDidUpdate(prevState, prevProps) {
-        
+        if ()
         DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
     }
 
