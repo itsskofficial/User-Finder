0584affee63d9364b03ad87df7105700a69a66c4
SK <sarthakkarandikar03@gmail.com>
2023-01-05 00:39:58 +0000

    Jan 5, 2023, 6:09 AM

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -2,7 +2,7 @@ import { Component } from "react/cjs/react.production.min";
 
 class ErrorBoundary extends Component {
     componentDidCatch(error) { 
-        this.setState
+        this.setState({hasError:true})
     }
     
     render() {
